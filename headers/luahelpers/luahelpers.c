1d2545fb63b632a79c85490db57935d927979390
Luc de wit <luc.dewit@hotmail.nl>
2020-07-07 19:18:43 +0200

    worked on lua helpers

--- headers/luahelpers/luahelpers.c
+++ headers/luahelpers/luahelpers.c
@@ -1,15 +1,29 @@
 #include "luahelpers.h"
 
-int luah_mapHasKey (lua_State* L, const char* map, const char *key) {
-    lua_getglobal(L, map); // push map to top of stack
-    lua_pushstring(L, key); // push key to top of stack
-    lua_gettable(L, -2);  /* get table back */
+/* assume that table is on the stack top */
+char* getfield (lua_State* L, const char *key) {
+    char* result;
+    lua_pushstring(L, key);
+    lua_gettable(L, -2);  /* get background[key] */
 
-    if (lua_isnil(L, -1)) {
-        lua_pop(L, 1);  /* remove number */
-        return 1;//true
-    } else {
-        lua_pop(L, 1);  /* remove number */
-        return 0;//false
-    }
+    if (lua_isnil(L, -1))
+        puts("i");
+
+    result = (char*)lua_tostring(L, -1);
+    lua_pop(L, 1);  /* remove number */
+    return result;
 }
+
+int luah_mapHasKey (lua_State* L, const char* map, const char *key) {
+
+
+    lua_getglobal(L, map);
+
+    if (lua_istable(L, -1))
+        printf("yeeee");
+
+    char* field = getfield(L, key);
+    printf("field = %s\n", field);
+    return 1;//true
+}
+
